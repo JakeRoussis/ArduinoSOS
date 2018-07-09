0be981a1f0f08e43860eb5c559e498fa91f391a0
JakeRoussis <40971419+JakeRoussis@users.noreply.github.com>
2018-07-09 14:34:07 +1000

    Update SOS.ino
    Correcting O delay (longer rather than shorter)

--- SOS.ino
+++ SOS.ino
@@ -18,15 +18,15 @@ void S() { // function to call S for SOS
 
 void O() { //function to call O for SOS
  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on
- delay(250); // wait quarter of a second
+ delay(1000); // wait 1 of a second
  digitalWrite(LED_BUILTIN, LOW); //turn LED off
- delay(500); //wait half a second
+ delay(1500); //wait 1.5 seconds
  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on
- delay(250); //wait quarter of a second
+ delay(1000); //wait 1 of a second
  digitalWrite(LED_BUILTIN, LOW); // turn LED off
- delay(500); //wait half a second
+ delay(1500); //wait 1.5 seconds
  digitalWrite(LED_BUILTIN, HIGH); // turn LED on
- delay(250); //wait quarter of a second
+ delay(1000); //wait 1 second
  digitalWrite(LED_BUILTIN, LOW); //turn LED off 
 }
 
@@ -34,14 +34,13 @@ void O() { //function to call O for SOS
 void loop() {
   //S for SOS
   S();
-  delay(1000);
+  delay(1500);
 
  //O for SOS
  O();
- delay(1000); //wait one second
+ delay(1500); //wait one second
  
  //S for SOS
  S();
  delay(3000); //long wait to show reset
 }
-
