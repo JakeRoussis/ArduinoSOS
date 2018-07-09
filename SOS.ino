0fd1cdaf356dfc4008bea53524f2edfaeead207f
JakeRoussis <40971419+JakeRoussis@users.noreply.github.com>
2018-07-09 14:26:49 +1000

    Upload SOS.ino
    Source code for Arduino SOS.

---
+++ SOS.ino
@@ -0,0 +1,47 @@
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void S() { // function to call S for SOS
+  digitalWrite(LED_BUILTIN, HIGH); // turn LED on
+  delay(500); // wait for half a second
+  digitalWrite(LED_BUILTIN, LOW); // turn LED off
+  delay(1000); // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH); // turn LED on
+  delay(500); // wait for half a second
+  digitalWrite(LED_BUILTIN, LOW); // turn LED off
+  delay(1000); // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH); // turn LED on
+  delay(500); // wait for half a second
+  digitalWrite(LED_BUILTIN, LOW); // turn LED off
+}
+
+void O() { //function to call O for SOS
+ digitalWrite(LED_BUILTIN, HIGH); // turn the LED on
+ delay(250); // wait quarter of a second
+ digitalWrite(LED_BUILTIN, LOW); //turn LED off
+ delay(500); //wait half a second
+ digitalWrite(LED_BUILTIN, HIGH); // turn the LED on
+ delay(250); //wait quarter of a second
+ digitalWrite(LED_BUILTIN, LOW); // turn LED off
+ delay(500); //wait half a second
+ digitalWrite(LED_BUILTIN, HIGH); // turn LED on
+ delay(250); //wait quarter of a second
+ digitalWrite(LED_BUILTIN, LOW); //turn LED off 
+}
+
+
+void loop() {
+  //S for SOS
+  S();
+  delay(1000);
+
+ //O for SOS
+ O();
+ delay(1000); //wait one second
+ 
+ //S for SOS
+ S();
+ delay(3000); //long wait to show reset
+}
+
